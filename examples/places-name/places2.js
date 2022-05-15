8b8c53ab53c319c8add8ba31404f153a039a2ba9
Ziyi WANG <ziyiwang1027@gmail.com>
2022-05-15 22:14:27 +0200

    init app

--- examples/places-name/places2.js
+++ examples/places-name/places2.js
@@ -879,13 +879,6 @@ window.onload = () => {
                     text.setAttribute('look-at', "[gps-camera]");
                     text.setAttribute('scale', "120 120 120");
                     text.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-                    text.addEventListener('click', () => {
-                        window.alert(place.name);
-                    });
-                    
-                    text.addEventListener('loaded', () => {
-                        window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
-                    });
                     scene.appendChild(text);
                 });
             })
@@ -898,66 +891,3 @@ window.onload = () => {
         }
     );
 };
-
-/*
-window.onload = () => {
-    const scene = document.querySelector('a-scene');
-
-    // first get current user location
-    return navigator.geolocation.getCurrentPosition(function (position) {
-
-            // then use it to load from remote APIs some places nearby
-            loadPlaces()
-                .then((places) => {
-                    places.forEach((place) => {
-                        const latitude = place.latitude;
-                        const longitude = place.longitude;
-
-                        // add place icon
-                        const icon = document.createElement('a-image');
-                        icon.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude}`);
-                        icon.setAttribute('name', place.name);
-                        icon.setAttribute('src', './examples/assets/map-marker.png');
-
-                        // for debug purposes, just show in a bigger scale, otherwise I have to personally go on places...
-                        icon.setAttribute('scale', '20, 20');
-
-                        icon.addEventListener('loaded', () => window.dispatchEvent(new CustomEvent('gps-entity-place-loaded')));
-
-                        const clickListener = function(ev) {
-                            ev.stopPropagation();
-                            ev.preventDefault();
-
-                            const name = ev.target.getAttribute('name');
-
-                            const el = ev.detail.intersection && ev.detail.intersection.object.el;
-
-                            if (el && el === ev.target) {
-                                const label = document.createElement('span');
-                                const container = document.createElement('div');
-                                container.setAttribute('id', 'place-label');
-                                label.innerText = name;
-                                container.appendChild(label);
-                                document.body.appendChild(container);
-
-                                setTimeout(() => {
-                                    container.parentElement.removeChild(container);
-                                }, 1500);
-                            }
-                        };
-
-                        icon.addEventListener('click',clickListener);
-
-                        scene.appendChild(icon);
-                    });
-                })
-        },
-        (err) => console.error('Error in retrieving position', err),
-        {
-            enableHighAccuracy: true,
-            maximumAge: 0,
-            timeout: 27000,
-        }
-    );
-};
-*/
