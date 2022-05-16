77aa91917f4eae06e500be8047062a4725f68ea5
Ziyi WANG <ziyiwang1027@gmail.com>
2022-05-16 10:40:35 +0200

    add label

--- examples/places-name/places2.js
+++ examples/places-name/places2.js
@@ -871,6 +871,31 @@ window.onload = () => {
                     text.addEventListener('loaded', () => {
                         window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
                     });
+
+                    const clickListener = function(ev) {
+                        ev.stopPropagation();
+                        ev.preventDefault();
+
+                        const name = ev.target.getAttribute('title');
+
+                        const el = ev.detail.intersection && ev.detail.intersection.object.el;
+
+                        if (el && el === ev.target) {
+                            const label = document.createElement('span');
+                            const container = document.createElement('div');
+                            container.setAttribute('id', 'place-label');
+                            label.innerText = name;
+                            container.appendChild(label);
+                            document.body.appendChild(container);
+
+                            setTimeout(() => {
+                                container.parentElement.removeChild(container);
+                            }, 1500);
+                        }
+                    };
+
+                    icon.addEventListener('click', clickListener);
+                    
                     scene.appendChild(text);
                     console.log("image ok");
 
