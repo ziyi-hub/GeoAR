1db63b41578c9434fb7a533065345c0e06bad198
Ziyi WANG <ziyiwang1027@gmail.com>
2022-05-24 00:50:15 +0200

    add map

--- js/map.js
+++ js/map.js
@@ -1,13 +1,36 @@
-function initialize()
-{
+function getData(){
+    debugger;
+    let parentWin = window.opener;
+    let scene = parentWin.document.querySelector('a-scene');
+    scene.querySelectorAll("a-link").forEach(link => {
+        console.log(link.dataset.description);
+    })
+}
+
+window.onload = getData;
+
+
+function initialize() {
     let mapProp = {
-        center:new google.maps.LatLng(48.68663079063300, 6.1794564225121),
-        zoom:12,
+        center:new google.maps.LatLng(48.684457, 6.163311),
+        zoom:4,
         mapTypeId:google.maps.MapTypeId.ROADMAP
     };
     map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+
+    let myLatLng = {lat: 48.684457, lng: 6.163311};
+    let marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+    });
+
+    let myLatLng2 = {lat: 50.684457, lng: 7.163311};
+    let marker2 = new google.maps.Marker({
+        position: myLatLng2,
+        map: map,
+    });
+
 }
 
-
-initialize();
+google.maps.event.addDomListener(window, 'load', initialize);
 
