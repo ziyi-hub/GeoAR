e088d9db3392bc18850a96ed86691196113d8b2f
Ziyi WANG <ziyiwang1027@gmail.com>
2022-05-24 10:48:00 +0200

    commit poiDetail

--- js/geo.js
+++ js/geo.js
@@ -56,7 +56,7 @@ window.onload = () => {
                         text.setAttribute('title', place.name);
                         text.setAttribute('data-description', place.description);
                         //text.setAttribute('href', place.image);
-                        text.setAttribute('href', "poiDetail.html");
+                        //text.setAttribute('href', "poiDetail.html");
                         text.setAttribute('scale', '120 120 120');
                         text.setAttribute('change-color-on-click', "")
 
