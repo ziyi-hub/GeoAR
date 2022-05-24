12b230009b64f4b747a9bbd15a985a8deef5f1b5
Ziyi WANG <ziyiwang1027@gmail.com>
2022-05-24 10:29:31 +0200

    update event

--- js/geo.js
+++ js/geo.js
@@ -75,3 +75,73 @@ window.onload = () => {
         }
     );
 };
+const list = document.querySelectorAll(".list");
+function activeLink(){
+    list.forEach((item)=>
+        item.classList.remove("active"));
+    this.classList.add("active");
+}
+list.forEach((item) => item.addEventListener('click', activeLink));
+
+
+const loadPlaces = function () {
+
+    let PLACES;
+    let request = new XMLHttpRequest();
+
+    request.open('GET','../datas/places.json', false);
+    request.onreadystatechange = function () {
+        if (this.readyState === 4) {
+            PLACES = JSON.parse(this.responseText);
+        }
+    };
+    request.send();
+    return Promise.resolve(PLACES);
+};
+
+
+AFRAME.registerComponent('change-color-on-click', {
+    init: function () {
+        let scene = document.querySelector('a-scene');
+        scene.querySelectorAll("a-link").forEach(link => {
+            link.onclick = () => {
+                const title = link.getAttribute("title");
+                alert(link.dataset.description);
+                //window.open(link.getAttribute("href"))
+            }
+        })
+    }
+});
+
+
+window.onload = () => {
+    const scene = document.querySelector('a-scene');
+
+    // first get current user location
+    return navigator.geolocation.getCurrentPosition(function (position) {
+
+            // than use it to load from remote APIs some places nearby
+            loadPlaces()
+                .then((places) => {
+                    places.forEach((place) => {
+                        const latitude = place.latitude;
+                        const longitude = place.longitude;
+
+                        // add place name
+                        const text = document.createElement('a-link');
+                        text.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+                        text.setAttribute('title', place.name);
+                        text.setAttribute('data-description', place.description);
+                        //text.setAttribute('href', place.image);
+                        //text.setAttribute('href', "poiDetail.html");
+                    });
+                })
+        },
+        (err) => console.error('Error in retrieving position', err),
+        {
+            enableHighAccuracy: true,
+            maximumAge: 0,
+            timeout: 27000,
+        }
+    );
+};
