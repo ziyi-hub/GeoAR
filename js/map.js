473564313453e3dccc7044a5411b84d9a392171e
Ziyi WANG <ziyiwang1027@gmail.com>
2022-05-25 15:47:22 +0200

    fix overflow

--- js/map.js
+++ js/map.js
@@ -39,8 +39,10 @@ let isMap = true;
 document.querySelector(".mdl-button").addEventListener("click", ()=>{
     isMap = !isMap;
     if(!isMap){
-        document.querySelector("#infos").style.visibility = "visible";
-        document.querySelector("#googleMap").style.visibility = "hidden";
+        //document.querySelector("#infos").style.visibility = "visible";
+        //document.querySelector("#googleMap").style.visibility = "hidden";
+        document.querySelector("#infos").style.display = "block";
+        document.querySelector("#googleMap").style.display = "none";
         document.querySelector(".material-icons").innerHTML = "location_on";
 
         /**
@@ -107,8 +109,10 @@ document.querySelector(".mdl-button").addEventListener("click", ()=>{
          * cacher informations et afficher map
          * @type {string}
          */
-        document.querySelector("#infos").style.visibility = "hidden";
-        document.querySelector("#googleMap").style.visibility = "visible";
+        //document.querySelector("#infos").style.visibility = "hidden";
+        //document.querySelector("#googleMap").style.visibility = "visible";
+        document.querySelector("#infos").style.display = "none";
+        document.querySelector("#googleMap").style.display = "block";
         document.querySelector(".material-icons").innerHTML = "event_note";
     }
 })
