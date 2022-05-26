bbb2642c751c1831ea3463e16624a5c933e74e35
Ziyi WANG <ziyiwang1027@gmail.com>
2022-05-26 23:05:22 +0200

    fix nav

--- js/geo.js
+++ js/geo.js
@@ -1,12 +1,3 @@
-const list = document.querySelectorAll(".list");
-function activeLink(){
-    list.forEach((item)=>
-        item.classList.remove("active"));
-    this.classList.add("active");
-}
-list.forEach((item) => item.addEventListener('click', activeLink));
-
-
 const loadPlaces = function () {
 
     let PLACES;
