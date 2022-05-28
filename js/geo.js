7efbb3d8f60135ed7f276a0732f7e96078f9f76b
Ziyi WANG <ziyiwang1027@gmail.com>
2022-05-28 23:03:54 +0200

    fix distance

--- js/geo.js
+++ js/geo.js
@@ -13,6 +13,7 @@ const loadPlaces = function () {
     return Promise.resolve(PLACES);
 };
 
+
 function getDistance(lat1, lng1, lat2, lng2){
     let radLat1 = lat1 * Math.PI/ 180.0 ;
     let radLat2 = lat2 * Math.PI/ 180.0 ;
@@ -24,6 +25,7 @@ function getDistance(lat1, lng1, lat2, lng2){
     return s;
 }
 
+
 function setCookie(cname, cvalue, exdays) {
     const d = new Date();
     d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
@@ -31,123 +33,101 @@ function setCookie(cname, cvalue, exdays) {
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
+function generatePOIS(places){
+    const scene = document.querySelector('a-scene');
+    places.forEach((place) => {
+        // add place name
+        const text = document.createElement('a-link');
+        text.setAttribute('gps-entity-place', `latitude: ${place.latitude}; longitude: ${place.longitude};`);
+        text.setAttribute('title', place.name);
+        text.setAttribute('data-id', place.id);
+        text.setAttribute('data-latitude', place.latitude);
+        text.setAttribute('data-longitude', place.longitude);
+        text.setAttribute('data-titre', place.name);
+        text.setAttribute('data-description', place.description);
+        text.setAttribute('data-image', place.image);
 
-AFRAME.registerComponent('open-window-on-click', {
-    init: function () {
-        let scene = document.querySelector('a-scene');
-        let latCur;
-        let lngCur;
-        
-        scene.querySelectorAll("a-link").forEach(link => {
-            link.onclick = (ev) => {
-                
-                ev.stopPropagation();
-                ev.preventDefault();
-                
-                if (navigator.geolocation)
-                {
-                    navigator.geolocation.getCurrentPosition((position)=>{
-                        latCur = position.coords.latitude;
-                        lngCur = position.coords.longitude;
-                    });
-                } else{
-                    alert("Geolocation is not supported by this browser.");
-                }
-                
-                let content = document.querySelector(".panel");
-                while (content.hasChildNodes()) {
-                    content.removeChild(content.firstChild);
-                }
-                
-                content.style.display = "block";
-                content.style.position = "absolute";
-                content.style.right = "0";
-                content.style.width = "320px";
-                
-                let p = document.createElement("p");
-                let p2 = document.createElement("p");
-                let p3 = document.createElement("p");
-                let close = document.createElement("span");
-                let button = document.createElement("button");
-                let img = document.createElement("img");
-                let latitude = link.dataset.latitude;
-                let longitude = link.dataset.longitude;
-                
-                img.className = "fit-picture";
-                img.src = link.dataset.image;
-                img.alt = link.dataset.image;
-                button.className = "mdl-button mdl-button--raised mdl-button--accent";
-                button.innerHTML = "Afficher plus";
-                p.className = "text-large";
-                p.innerHTML = link.dataset.titre;
-                p2.innerHTML = link.dataset.description;
-                p3.innerHTML = "Distances: " + getDistance(latitude, longitude, latCur, lngCur) + " km";
-                close.className = "text-large close";
-                close.innerHTML = "&#x2718;";
-                
-                close.addEventListener("click", ()=>{
-                    content.style.display = "none";
-                })
+        text.setAttribute('href', "javascript:void(0)");
+        text.setAttribute('scale', '120 120 120');
+        text.setAttribute('open-window-on-click', "")
 
-                document.querySelector(".panel").appendChild(close);
-                document.querySelector(".panel").appendChild(p);
-                document.querySelector(".panel").appendChild(p2);
-                document.querySelector(".panel").appendChild(p3);
-                document.querySelector(".panel").appendChild(button);
-                document.querySelector(".panel").appendChild(img);
-
-                document.querySelector('.mdl-button').addEventListener("click", ()=>{
-                    setCookie("id", link.dataset.id, 1);
-                    window.location.href = "poiDetail.html";
-                })
-            }
-        })
-    }
-});
+        text.addEventListener('loaded', () => {
+            window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
+        });
+        scene.appendChild(text);
+    });
+}
 
 
 window.onload = () => {
-    const scene = document.querySelector('a-scene');
 
     // first get current user location
     return navigator.geolocation.getCurrentPosition(function (position) {
 
+            AFRAME.registerComponent('open-window-on-click', {
+                init: function () {
+                    let scene = document.querySelector('a-scene');
+                    scene.querySelectorAll("a-link").forEach(link => {
+                        link.onclick = (ev) => {
+
+                            ev.stopPropagation();
+                            ev.preventDefault();
+
+                            let content = document.querySelector(".panel");
+                            while (content.hasChildNodes()) {
+                                content.removeChild(content.firstChild);
+                            }
+
+                            content.style.display = "block";
+                            content.style.position = "absolute";
+                            content.style.right = "0";
+                            content.style.width = "320px";
+
+                            let p = document.createElement("p");
+                            let p2 = document.createElement("p");
+                            let p3 = document.createElement("p");
+                            let close = document.createElement("span");
+                            let button = document.createElement("button");
+                            let img = document.createElement("img");
+                            let latitude = link.dataset.latitude;
+                            let longitude = link.dataset.longitude;
+
+                            img.className = "fit-picture";
+                            img.src = link.dataset.image;
+                            img.alt = link.dataset.image;
+                            button.className = "mdl-button mdl-button--raised mdl-button--accent";
+                            button.innerHTML = "Afficher plus";
+                            p.className = "text-large";
+                            p.innerHTML = link.dataset.titre;
+                            p2.innerHTML = link.dataset.description;
+                            p3.innerHTML = "Distances: " + getDistance(latitude, longitude, position.coords.latitude, position.coords.longitude) +" km";
+                            close.className = "text-large close";
+                            close.innerHTML = "&#x2718;";
+
+                            close.addEventListener("click", ()=>{
+                                content.style.display = "none";
+                            })
+
+                            document.querySelector(".panel").appendChild(close);
+                            document.querySelector(".panel").appendChild(p);
+                            document.querySelector(".panel").appendChild(p2);
+                            document.querySelector(".panel").appendChild(p3);
+                            document.querySelector(".panel").appendChild(button);
+                            document.querySelector(".panel").appendChild(img);
+
+                            document.querySelector('.mdl-button').addEventListener("click", ()=>{
+                                setCookie("id", link.dataset.id, 1);
+                                window.location.href = "poiDetail.html";
+                            })
+                        }
+                    })
+                }
+            });
+
             // than use it to load from remote APIs some places nearby
             loadPlaces()
                 .then((places) => {
-                    places.forEach((place) => {
-                        const latitude = place.latitude;
-                        const longitude = place.longitude;
-
-                        // add place name
-                        const text = document.createElement('a-link');
-                        text.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-                        text.setAttribute('title', place.name);
-                        text.setAttribute('data-id', place.id);
-                        text.setAttribute('data-latitude', place.latitude);
-                        text.setAttribute('data-longitude', place.longitude);
-                        text.setAttribute('data-titre', place.name);
-                        text.setAttribute('data-description', place.description);
-                        text.setAttribute('data-image', place.image);
-
-                        text.setAttribute('href', "javascript:void(0)");
-                        text.setAttribute('scale', '120 120 120');
-                        text.setAttribute('open-window-on-click', "")
-
-                        text.addEventListener('loaded', () => {
-                            window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
-                        });
-
-                        /*
-                        const text = document.createElement('a-image');
-                        text.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude}`);
-                        text.setAttribute('name', place.name);
-                        text.setAttribute('data-description', place.description);
-                        text.setAttribute('src', place.image);
-                        text.setAttribute('scale', '20, 20');
-                        */
-                        scene.appendChild(text);
-                    });
+                    generatePOIS(places);
                 })
         },
         (err) => console.error('Error in retrieving position', err),
