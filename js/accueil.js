cd06256cdd876b5c0eb9743a48dbf76d34a529fa
Ziyi WANG <ziyiwang1027@gmail.com>
2022-05-30 00:14:01 +0200

    init accueil

---
+++ js/accueil.js
@@ -0,0 +1,61 @@
+function selectionSort(arr) {
+    let len = arr.length;
+    let minIndex, temp;
+    for (let i = 0; i < len - 1; i++) {
+        minIndex = i;
+        for (let j = i + 1; j < len; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
+    }
+    return arr;
+}
+
+function getDistance(lat1, lng1, lat2, lng2){
+    let radLat1 = lat1 * Math.PI/ 180.0 ;
+    let radLat2 = lat2 * Math.PI/ 180.0 ;
+    let a = radLat1 - radLat2;
+    let b = lng1 * Math.PI/ 180.0 - lng2 * Math.PI/ 180.0 ;
+    let s = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(a / 2), 2) + Math.cos(radLat1) * Math.cos(radLat2) * Math.pow(Math.sin(b / 2), 2)))
+    s = s * 6378.137; //Earth radius
+    s = Math.round(s * 10000) / 10000;
+    return s.toFixed(2);
+}
+
+function loadPlaces() {
+    let PLACES;
+    let request = new XMLHttpRequest();
+
+    request.open('GET','https://ziyi-hub.github.io/GeoAR/datas/places.json', false);
+    request.onreadystatechange = function () {
+        if (this.readyState === 4) {
+            PLACES = JSON.parse(this.responseText);
+        }
+    };
+    request.send();
+    return Promise.resolve(PLACES);
+};
+
+
+window.onload = () => {
+    let arr = [];
+    
+    // first get current user location
+    return navigator.geolocation.getCurrentPosition(function (position) {
+        loadPlaces()
+            .then((places) => {
+                places.forEach((place) => {
+                    let dis = getDistance(place.latitude, place.longitude, position.coords.latitude, position.coords.longitude);
+                    arr.push(dis);
+                });
+                selectionSort(arr);
+            });
+        console.log(arr);
+    })
+
+}
+
