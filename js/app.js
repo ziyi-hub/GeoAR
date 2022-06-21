d2675ae07c5b5a5cea20b099d469d8106ae8f112
Ziyi WANG <ziyiwang1027@gmail.com>
2022-06-21 14:47:01 +0200

    init app

--- js/app.js
+++ js/app.js
@@ -2,9 +2,10 @@ if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('serviceWorker.js')
         .then(function(reg){
             console.log("service worker registered")
-        }).catch(function(err) {
-        console.log(err)
-    });
+        })
+        .catch(function(err) {
+            console.log(err)
+        });
 } else {
     console.log("Could not find serviceWorker in navigator")
 }
