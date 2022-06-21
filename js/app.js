61c0da6e668f78c9a7756c70ce87c56afcfbff9b
Ziyi WANG <ziyiwang1027@gmail.com>
2022-06-21 13:09:23 +0200

    init app

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('serviceWorker.js');
 }
+
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+    // Update UI notify the user they can add to home screen
+    document.querySelector('#install').style.display = 'block';
+});
