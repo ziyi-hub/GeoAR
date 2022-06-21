19433b30d9434ace228582e7950dfc9b761aead3
Ziyi WANG <ziyiwang1027@gmail.com>
2022-06-21 14:06:46 +0200

    add fonctionnalité supprime cache

--- serviceWorker.js
+++ serviceWorker.js
@@ -48,18 +48,17 @@ self.addEventListener('fetch', (event)=>{
             })
     );
 });
-/*
-self.addEventListener('activate', (event) => {
-    console.log('[ServiceWorker] Activate');
-    event.waitUntil((async () => {
-        // Enable navigation preload if it's supported.
-        // See https://developers.google.com/web/updates/2017/02/navigation-preload
-        if ('navigationPreload' in self.registration) {
-            await self.registration.navigationPreload.enable();
-        }
-    })());
-    // Tell the active service worker to take control of the page immediately.
-    self.clients.claim();
-});
 
- */
+//supprimer cache
+self.addEventListener('activate', (e)=>{
+    e.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.add(
+                keys.filter((key) => key !== staticCacheName).map((key => caches.delete(key)))
+            )
+        })
+            .catch(err => {
+                console.log(err.message);
+            })
+    )
+})
