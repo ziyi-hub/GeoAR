d263112dcf1391bf1471a392538341903f82a26d
Ziyi WANG <ziyiwang1027@gmail.com>
2022-06-23 17:11:45 +0200

    init app

--- js/geo.js
+++ js/geo.js
@@ -1,3 +1,9 @@
+let images = [
+    "../assets/splashscreen/logo.png",
+    "../assets/image/installation_1.png",
+    "../assets/image/intro2.jpg"
+];
+
 /**
  * il permet de récupérer des datas
  * @param url url d'un fichier data
@@ -110,8 +116,110 @@ function getCurPosition(options) {
     });
 }
 
+function closeModal(){
+    document.querySelector(".d-modal").style.display = "none";
+}
+
+function createSlide(nbImage){
+    let slideContainer = document.querySelector(".slide-container");
+
+    while (slideContainer.hasChildNodes()) {
+        slideContainer.removeChild(slideContainer.firstChild);
+    }
+
+    let prev = document.createElement("a");
+    prev.className = "prev";
+    prev.innerHTML = "&#10094;";
+
+    let next = document.createElement("a");
+    next.className = "next";
+    next.innerHTML = "&#10095;";
+
+    for (let i = 0; i < nbImage; i++){
+        let customSlider = document.createElement("div");
+        customSlider.className = "custom-slider fade";
+
+        let slideIndex = document.createElement("div");
+        slideIndex.className = "slide-index";
+        slideIndex.innerHTML = `${i + 1}/${nbImage}`;
+
+        let slideImg = document.createElement("img");
+        slideImg.className = "slide-img";
+        slideImg.src = images[i];
+
+        customSlider.appendChild(slideIndex);
+        customSlider.appendChild(slideImg);
+        slideContainer.appendChild(customSlider);
+    }
+    slideContainer.appendChild(prev);
+    slideContainer.appendChild(next);
+}
+
+
+function createDot(nbImage){
+    let slideDot = document.querySelector(".slide-dot");
+
+    while (slideDot.hasChildNodes()) {
+        slideDot.removeChild(slideDot.firstChild);
+    }
+
+    for (let i = 0; i < nbImage; i++){
+        let span = document.createElement("span");
+        span.className = "dot";
+        slideDot.appendChild(span);
+    }
+}
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showBtnDemarrer(n){
+    if (n === images.length){
+        document.querySelector(".d-btn").style.display = "inline-block";
+    }else{
+        document.querySelector(".d-btn").style.display = "none";
+    }
+}
+
+let slideIndex = 1;
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("custom-slider");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active2", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active2";
+    showBtnDemarrer(n);
+}
 
 window.onload = () => {
+    document.querySelector(".d-modal-head-right").addEventListener("click", closeModal);
+    document.querySelector(".d-btn").addEventListener("click", closeModal);
+    
+    // images slide
+    createSlide(images.length);
+    createDot(images.length);
+    
+    showSlides(slideIndex);
+    document.querySelector(".prev").addEventListener('click', ()=>{plusSlides(-1)})
+    document.querySelector(".next").addEventListener('click', ()=>{plusSlides(1)})
+    for(let i = 0; i < document.querySelectorAll(".dot").length; i++){
+        document.querySelectorAll(".dot")[i].addEventListener("click", ()=>{currentSlide(i+1);})
+    }
+    
+    //AFRAME
     AFRAME.registerComponent('open-window-on-click', {
         init: function () {
             this.el.addEventListener('click', () => {
