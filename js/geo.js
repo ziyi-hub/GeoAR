959315df6a3e1611956719260eb6252730491ac9
Ziyi WANG <ziyiwang1027@gmail.com>
2022-06-29 17:34:27 +0200

    fix camera

--- js/geo.js
+++ js/geo.js
@@ -135,6 +135,14 @@ function showSlidesGeo(n) {
     dots[slideIndex-1].className += " active2";
 }
 
+navigator.permissions.query({name: 'camera'})
+    .then((permissionObj) => {
+        console.log(permissionObj.state);
+    })
+    .catch((error) => {
+        console.log('Got error :', error);
+    })
+
 //window.onload = () => {
     (localStorage.getItem('closeModal') === "true")?closeModal():null;
 
