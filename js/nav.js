aa3aa42f818c8d284470c2c37b24912714cdbc79
Ziyi WANG <ziyiwang1027@gmail.com>
2022-07-11 15:11:06 +0200

    init app

--- js/nav.js
+++ js/nav.js
@@ -1,6 +1,9 @@
 const list = document.querySelectorAll(".list");
 list.forEach((item) => item.addEventListener('click', activeLink));
 
+/**
+ * Il permet au pilote de naviguer à travers les pages
+ */
 function activeLink(){
     list.forEach((item)=>
         item.classList.remove("active"));
